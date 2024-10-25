e553f1c9823b288e201c2e7e6f6e9c55dcfd1413
Ricardo <ricardo.chomicz@gmail.com>
2024-10-25 15:20:12 -0300

    refactor: udpdate create view command

---
+++ resources/views/commands/create.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+@include('layouts.navbar');
+
+@section('content')
+    <div
+        class="w-full p-4 text-center bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700 mt-10">
+        <form class="max-w-sm mx-auto" action="{{ route('commands.store') }}" method="POST">
+            @csrf
+            @include('commands._partials.form')
+        </form>
+    </div>
+@stop
