fbf25a14af854850380cb1c7893b304b0c36adc4
Ricardo <ricardo.chomicz@gmail.com>
2024-10-25 18:38:32 -0300

    refactor: update view create command

--- resources/views/commands/create.blade.php
+++ resources/views/commands/create.blade.php
@@ -3,7 +3,7 @@
 
 @section('content')
     <div
-        class="w-full p-4 text-center bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700 mt-10">
+        class="w-full p-4 text-center bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
         <form class="max-w-sm mx-auto" action="{{ route('commands.store') }}" method="POST">
             @csrf
             @include('commands._partials.form')
