7ee2c57a0f7760a94b9766cf66c52f4890efa61c
Ricardo <ricardo.chomicz@gmail.com>
2024-10-25 18:42:08 -0300

    refactor: update routes

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'app'], function () {
+    Route::get('commands/create', [App\Http\Controllers\CommandController::class, 'create'])->name('commands.create');
+    Route::post('commands', [App\Http\Controllers\CommandController::class, 'store'])->name('commands.store');
     Route::get('commands', [App\Http\Controllers\CommandController::class, 'index'])->name('commands.index');
 });
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.app');
 });
