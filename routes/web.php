05c8b29d1c9254a032ea3a21d92c6f893f138bfa
Ricardo <ricardo.chomicz@gmail.com>
2024-10-25 10:16:14 -0300

    refactor: commands index

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::group(['prefix' => 'app'], function () {
+    Route::get('commands', [App\Http\Controllers\CommandController::class, 'index'])->name('commands.index');
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
